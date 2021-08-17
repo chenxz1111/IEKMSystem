5d4e9eda09301a4aa0ea4eadf6d2c7fed649dab3
chenxz <792146066@qq.com>
2021-08-17 23:27:40 +0800

    login successfully

--- app/src/main/java/com/example/wangbotian/MainActivity.java
+++ app/src/main/java/com/example/wangbotian/MainActivity.java
@@ -1,11 +1,16 @@
 package com.example.wangbotian;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Activity;
+import android.app.Application;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.content.Intent;
+
+import com.hjq.xtoast.XToast;
+
+
 
 public class MainActivity extends AppCompatActivity {
 
