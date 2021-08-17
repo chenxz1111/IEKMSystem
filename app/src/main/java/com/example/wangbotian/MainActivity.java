202051a53064d63867d30ea7332368eea68e400b
甘乔尹 <2019011240@secoder.net>
2021-08-17 21:55:59 +0800

    add logic of login

--- app/src/main/java/com/example/wangbotian/MainActivity.java
+++ app/src/main/java/com/example/wangbotian/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.wangbotian;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
 
