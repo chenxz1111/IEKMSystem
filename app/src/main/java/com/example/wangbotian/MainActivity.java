1a27dfa846e47bfa4e1d19e3060ffdf42e465e93
chenxz <792146066@qq.com>
2021-08-18 12:45:40 +0800

    add bottom navigation bar

--- app/src/main/java/com/example/wangbotian/MainActivity.java
+++ app/src/main/java/com/example/wangbotian/MainActivity.java
@@ -1,22 +1,22 @@
 package com.example.wangbotian;
 
 import androidx.appcompat.app.AppCompatActivity;
-import android.app.Activity;
-import android.app.Application;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.content.Intent;
 
-import com.hjq.xtoast.XToast;
-
+import io.github.vejei.bottomnavigationbar.BottomNavigationBar;
 
 
 public class MainActivity extends AppCompatActivity {
-
+    private BottomNavigationBar bottomNavigationBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
     }
+    private void initView() {
+        //bottomNavigationBar = BottomNavigationBar.
+    }
+
 }
+
