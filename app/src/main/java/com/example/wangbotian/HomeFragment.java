e336f9bc54e5b0e2829119dd767df0b3f0287ffe
wbt <2398771367@qq.com>
2021-08-21 22:37:54 +0800

    Add basic tab-layout demo

--- app/src/main/java/com/example/wangbotian/HomeFragment.java
+++ app/src/main/java/com/example/wangbotian/HomeFragment.java
@@ -1,12 +1,17 @@
 package com.example.wangbotian;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
 
 public class HomeFragment extends Fragment {
 
@@ -19,7 +24,8 @@ public class HomeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+        View v = inflater.inflate(R.layout.fragment_home, container, false);
+
         return inflater.inflate(R.layout.fragment_home, container, false);
     }
 }
