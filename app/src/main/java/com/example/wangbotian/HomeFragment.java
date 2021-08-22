4ffc40fef0988a08c34b54f7949d3e78eff1a883
wbt <2398771367@qq.com>
2021-08-23 00:18:19 +0800

    Add basic tab-layout

--- app/src/main/java/com/example/wangbotian/HomeFragment.java
+++ app/src/main/java/com/example/wangbotian/HomeFragment.java
@@ -11,21 +11,43 @@ import androidx.viewpager.widget.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.google.android.material.tabs.TabLayout;
+
+import java.util.*;
 
 
 public class HomeFragment extends Fragment {
-
-
-    public HomeFragment() {
-        // Required empty public constructor
-    }
-
+    private ViewPager pager;
+    private FragmentAdapter fragmentAdapter;
+    private List<TabFragment> fragmentList;
+    private TabLayout tabLayout;
+    private List<String> mTitles;
+    private String [] title={"语文","数学","英语","物理","化学","生物","历史","地理","政治"};
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        pager=view.findViewById(R.id.page);
+        tabLayout=view.findViewById(R.id.tab_layout);
+        return view;
+    }
 
-        return inflater.inflate(R.layout.fragment_home, container, false);
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState){
+        super.onActivityCreated(savedInstanceState);
+
+        fragmentList=new ArrayList<>();
+        mTitles=new ArrayList<>();
+        for(int i=0;i<title.length;i++){
+            mTitles.add(title[i]);
+            fragmentList.add(new TabFragment(title[i]));
+        }
+
+        fragmentAdapter=new FragmentAdapter(getActivity().getSupportFragmentManager(),fragmentList,mTitles, FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+        pager.setAdapter(fragmentAdapter);
+        tabLayout.setupWithViewPager(pager);//与ViewPage建立关系
     }
 }
