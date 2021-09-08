103426646dc88983e189c59706b649d03a1e8544
chenxz <792146066@qq.com>
2021-09-09 02:06:02 +0800

    icon

--- app/src/main/java/com/example/wangbotian/EntityPropertyFragment.java
+++ app/src/main/java/com/example/wangbotian/EntityPropertyFragment.java
@@ -1,9 +1,13 @@
 package com.example.wangbotian;
 
+import static android.content.Context.MODE_PRIVATE;
+
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,12 +15,14 @@ import android.widget.ListView;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 
 public class EntityPropertyFragment extends Fragment {
 
     ListView propertyList;
-
+    private final String PREFS_NAME = "MyPrefsFile";
     public EntityPropertyFragment() {
 
     }
@@ -29,8 +35,16 @@ public class EntityPropertyFragment extends Fragment {
         propertyList = v.findViewById(R.id.entity_property_list);
         EntityActivity entity = (EntityActivity)this.getActivity();
         EntityItem[] items = new EntityItem[entity.entityProperty.size()];
+        SharedPreferences history = getActivity().getSharedPreferences(PREFS_NAME, MODE_PRIVATE);
+        Set<String> listHistory = history.getStringSet("entity_list_history", null);
+        if(listHistory == null) {
+            listHistory = new HashSet<String>();
+        }
         for(int i = 0; i < entity.entityProperty.size(); i++) {
             items[i] = new EntityItem(entity.entityProperty.getJSONObject(i).getString("predicateLabel"), entity.entityProperty.getJSONObject(i).getString("object"));
+            if(listHistory.contains(entity.entityProperty.getJSONObject(i).getString("predicateLabel") + ";" + entity.entityProperty.getJSONObject(i).getString("object"))) {
+                items[i].access();
+            }
         }
         ArrayList<EntityItem> items_list = new ArrayList<EntityItem>(Arrays.asList(items));
         EntityListAdapter adapter = new EntityListAdapter(this.getActivity(), items_list);
--- app/src/main/java/com/example/wangbotian/EntityRelationFragment.java
+++ app/src/main/java/com/example/wangbotian/EntityRelationFragment.java
@@ -1,5 +1,8 @@
 package com.example.wangbotian;
 
+import static android.content.Context.MODE_PRIVATE;
+
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -11,12 +14,14 @@ import android.widget.ListView;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 
 public class EntityRelationFragment extends Fragment {
 
     ListView relationList;
-
+    private final String PREFS_NAME = "MyPrefsFile";
     public EntityRelationFragment() {
 
     }
@@ -29,11 +34,25 @@ public class EntityRelationFragment extends Fragment {
         relationList = v.findViewById(R.id.entity_relation_list);
         EntityActivity entity = (EntityActivity)this.getActivity();
         EntityItem[] items = new EntityItem[entity.entityRelation.size()];
+        SharedPreferences history = getActivity().getSharedPreferences(PREFS_NAME, MODE_PRIVATE);
+        Set<String> listHistory = history.getStringSet("entity_list_history", null);
+        if(listHistory == null) {
+            listHistory = new HashSet<String>();
+        }
         for(int i = 0; i < entity.entityRelation.size(); i++) {
-            if (entity.entityRelation.getJSONObject(i).containsKey("subject"))
-                items[i] = new EntityItem(entity.entityRelation.getJSONObject(i).getString("subject_label"), entity.entityRelation.getJSONObject(i).getString("predicate_label"));
-            else
-                items[i] = new EntityItem(entity.entityRelation.getJSONObject(i).getString("object_label"), entity.entityRelation.getJSONObject(i).getString("predicate_label"));
+            String tmp;
+            if (entity.entityRelation.getJSONObject(i).containsKey("subject")) {
+                tmp = entity.entityRelation.getJSONObject(i).getString("subject_label");
+                items[i] = new EntityItem(tmp, entity.entityRelation.getJSONObject(i).getString("predicate_label"));
+            }
+
+            else {
+                tmp = entity.entityRelation.getJSONObject(i).getString("object_label");
+                items[i] = new EntityItem(tmp, entity.entityRelation.getJSONObject(i).getString("predicate_label"));
+            }
+            if(listHistory.contains(tmp + ";" + entity.entityRelation.getJSONObject(i).getString("predicate_label"))) {
+                items[i].access();
+            }
         }
         ArrayList<EntityItem> items_list = new ArrayList<EntityItem>(Arrays.asList(items));
         EntityListAdapter adapter = new EntityListAdapter(this.getActivity(), items_list);
